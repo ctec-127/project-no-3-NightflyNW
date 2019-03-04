8fa092133fa713b7374cf4244057759c28dfdb00
scott <s.altom@students.clark.edu>
2019-03-04 11:56:35 -0800

    Section 3 updates

--- inc/functions/functions.inc.php
+++ inc/functions/functions.inc.php
@@ -31,7 +31,17 @@ function display_letter_filters($filter){
 function display_record_table($result){
     echo '<div class="table-responsive">';
     echo "<table class=\"table table-striped table-hover table-sm mt-4\">";
-    echo '<thead class="thead-dark"><tr><th>Actions</th><th><a href="?sortby=id">ID</a></th><th><a href="?sortby=student_id">Student ID</a></th><th><a href="?sortby=first_name">First Name</a></th><th><a href="?sortby=last_name">Last Name</a></th><th><a href="?sortby=email">Email</a></th><th><a href="?sortby=phone">Phone</a></th><th><a href="?sortby=gpa">GPA</a></th><th><a href="?sortby=financial_aid">Financial Aid</a></th><th><a href="?sortby=degree_program">Degree Program</a></th><th><a href="?sortby=data_created">Data Created</a></th></tr></thead>';
+    // added GPA, Financial Aid, Degree Program Header
+    echo '<thead class="thead-dark"><tr><th>Actions</th>
+        <th><a href="?sortby=id">ID</a></th>
+        <th><a href="?sortby=student_id">Student ID</a></th>
+        <th><a href="?sortby=first_name">First Name</a></th>
+        <th><a href="?sortby=last_name">Last Name</a></th>
+        <th><a href="?sortby=email">Email</a></th>
+        <th><a href="?sortby=phone">Phone</a></th>
+        <th><a href="?sortby=gpa">GPA</a></th>
+        <th><a href="?sortby=financial_aid">Financial Aid</a></th>
+        <th><a href="?sortby=degree_program">Degree Program</a></th></thead>';
     # $row will be an associative array containing one row of data at a time
     while ($row = $result->fetch_assoc()){
         # display rows and columns of data
@@ -43,11 +53,11 @@ function display_record_table($result){
         echo "<td><strong>{$row['last_name']}</strong></td>";
         echo "<td>{$row['email']}</td>";
         echo "<td>{$row['phone']}</td>";
-        // added new database fields
+        // added new display data
         echo "<td>{$row['gpa']}</td>";
         echo "<td>{$row['financial_aid']}</td>";
         echo "<td>{$row['degree_program']}</td>";
-        echo "<td>{$row['data_created']}</td>";
+        
         
         // added new database fields
         
